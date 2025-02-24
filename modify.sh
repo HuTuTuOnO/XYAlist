2ef6d3a53710d4f5b379e367ca91ce966282e6bf
HuTuTu <122086569+HuTuTuOnO@users.noreply.github.com>
2025-02-25 05:16:28 +0800

    Update modify.sh

--- modify.sh
+++ modify.sh
@@ -1,1 +1,33 @@
+#!/bin/bash
 
+# 数据库文件路径
+DB_PATH="data.db"
+
+# 更新数据库
+sqlite3 $DB_PATH <<EOF
+UPDATE x_setting_items
+SET value = '$version'
+WHERE key = 'version';
+
+UPDATE x_setting_items
+SET value = '$site_title'
+WHERE key = 'site_title';
+
+UPDATE x_setting_items
+SET value = '$logo'
+WHERE key = 'logo';
+
+UPDATE x_setting_items
+SET value = '$hide_files'
+WHERE key = 'hide_files';
+
+UPDATE x_meta
+SET hide = ''
+WHERE hide IS NOT NULL AND hide != '';
+
+UPDATE x_meta
+SET readme = ''
+WHERE readme IS NOT NULL AND readme != '';
+EOF
+
+echo "更新完成！"
